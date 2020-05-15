e67637b6c0e115717a2521b56e4b6a53ca9874e4
Wes <wesleyayer92@gmail.com>
2020-05-15 10:21:56 -0400

    grabs and prints some basic info from weatherstack

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const { APIKEY } = require('./config');
 const request = require('request');
-const URL = `http://api.weatherstack.com/current?access_key=${APIKEY}&query=37.8267,-122.4233`;
-request({ url: URL }, (err, res) => {
-    const data = JSON.parse(res.body);
-    console.log(data);
+const URL = `http://api.weatherstack.com/current?access_key=${APIKEY}&query=37.8267,-122.4233&units=f`;
+request({ url: URL, json: true }, (err, res) => {
+    console.log(`${res.body.current.weather_descriptions[0]}. It is currently ${res.body.current.temperature} degrees.
+It feels like ${res.body.current.feelslike}`);
 });
