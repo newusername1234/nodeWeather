8123ff13f5b569f65202a4eb59e6594e2a98c102
Wes <wesleyayer92@gmail.com>
2020-05-20 10:58:56 -0400

    adds mapbox api call for lat long

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-const { APIKEY } = require('./config');
+const { WEATHERKEY, MAPKEY } = require('./config');
 const request = require('request');
-const URL = `http://api.weatherstack.com/current?access_key=${APIKEY}&query=37.8267,-122.4233&units=f`;
-request({ url: URL, json: true }, (err, res) => {
+const weatherURL = `http://api.weatherstack.com/current?access_key=${WEATHERKEY}&query=37.8267,-122.4233&units=f`;
+const mapURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${MAPKEY}&limit=1`;
+
+request({ url: weatherURL, json: true }, (err, res) => {
     console.log(`${res.body.current.weather_descriptions[0]}. It is currently ${res.body.current.temperature} degrees.
 It feels like ${res.body.current.feelslike}`);
+});
+
+request({url: mapURL, json: true}, (err, res) => {
+    const latLongArr = res.body.features[0].center;
+    console.log(`lat: ${latLongArr[0]}\nlong: ${latLongArr[1]}`)
 });
